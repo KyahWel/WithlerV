451a4354d7f7f943a7e1e6ca2eb29b042a4fd4ad
William <williamcris18@gmail.com>
2022-05-19 01:48:56 +0800

    Updated functionality

--- js/easy.js
+++ js/easy.js
@@ -22,6 +22,7 @@ const MAX_CONTAINER = 6; //PALITAN KUNG ILANG CONTAINER
 
 let currentContainer = 0;
 let inputIndex = 0;
+let streak = 0;
 var Answer = '';
 for (const button of buttons) {
     button.onclick = buttonHandler;
@@ -38,53 +39,90 @@ function buttonHandler(event) {
                 if(Answer.length < MAX_BOX) Answer += event.target.textContent
                 inputIndex++;
 
-                if (inputIndex == 5) inputIndex = 4;
+                if (inputIndex == 5) inputIndex = 5;
 
             }
-            console.log("Input index: "+inputIndex)
-            console.log("Answer: "+Answer)
+           
+          
         } else if (event.target.id === 'delete-btn') {
 
-            if (inputIndex > 0 && inputIndex <= MAX_BOX-1) inputIndex--;
+            if (inputIndex > 0 && inputIndex <= MAX_BOX) inputIndex--;
             
             if (inputIndex <= 0) inputIndex = 0;
-            if(inputIndex == MAX_BOX-1){
-                currentBox.children[0].textContent = '';
-                 currentBox = boxContainers[currentContainer].children[inputIndex];
-            }
-            else{     
-                currentBox = boxContainers[currentContainer].children[inputIndex];
-                currentBox.children[0].textContent = '';
-            }
-            
            
-            
+            currentBox = boxContainers[currentContainer].children[inputIndex];
+            currentBox.children[0].textContent = '';
             
             Answer = Answer.slice(0, Answer.length - 1);
-            console.log("Delete index: "+inputIndex)
-            console.log("Answer: "+Answer)
-           
-
+        
         } else if (event.target.id === 'reset-btn') {
 
 
         } else if (event.target.id === 'enter-btn') {
-
+            const answerArray = Answer.split("");
+            const expressionArray = expression.split("");
             numberOfTries++;
-            var result = evaluateAnswer(Answer)
-            if (result == true){
-                var evaluateResult = "Correct";
+            if (evaluateAnswer(Answer) == 1){
+               const opt = "1234567890+-/*"
+               for(let i = 0; i<MAX_BOX;i++){
+                currentBox = boxContainers[currentContainer].children[i];
+                currentBox.style.backgroundColor = "#3AA346";
+               }
+               for(let x = currentContainer+1; x<MAX_CONTAINER;x++){
+                   for (let y = 0; y<MAX_BOX; y++){
+                    currentBox = boxContainers[x].children[y];
+                    currentBox.style.backgroundColor = "#BBC6CD";
+                   }
+                
+               }
+               for (let z = 0; z < answerArray.length ; z++){
+                document.getElementById(answerArray[z]).style.background = "#3AA346";
+               }
+               for (let z = 0 ; z < opt.length ; z++){
+                document.getElementById(opt[z]).disabled = true;
+               }
+               currentContainer = MAX_CONTAINER;
+               document.getElementById('enter-btn').disabled = true;
+               document.getElementById('delete-btn').disabled = true;
+               document.getElementById('delete-btn').style.background = "#e25252"
+               streak++;
+               var myModal = new bootstrap.Modal(document.getElementById('correctModal'),{
+                    keyboard: false
+                  })
+                myModal.toggle()
+                $("#streak").text(streak)
+                continueGame();
             }
-            else var evaluateResult = "Wrong";
-
-            console.log("Test Answer: " + Answer + "\nExpression: " + expression + "\nAnswer: " + eval(expression) + "\nEvaluation: " + evaluateResult + "\nNumber of tries: " + numberOfTries)
-            inputIndex = 0;
-
-            Answer = '';
-            currentContainer++;
-            console.log(currentContainer)
-
-
+            else if(evaluateAnswer(Answer)== -1) {
+                var myModal = new bootstrap.Modal(document.getElementById('errorModal'),{
+                    keyboard: false
+                  })
+                myModal.toggle()
+                $("#answer").text(eval(expression))
+                
+            }
+            else {
+                for (let i = 0 ; i < answerArray.length ; i++){
+                    currentBox = boxContainers[currentContainer].children[i];
+                    var position = expression.indexOf(answerArray[i])
+                    if(expressionArray[i] == answerArray[i]){
+                        currentBox.style.backgroundColor = "#3AA346";
+                        document.getElementById(answerArray[i]).style.background = "#3AA346";
+                    }
+                    else if (expressionArray.includes(answerArray[i])){
+                        currentBox.style.backgroundColor = "#FFC30B";
+                        document.getElementById(answerArray[i]).style.background = "#FFC30B";    
+                    }
+                    else{
+                        currentBox.style.backgroundColor = "#e25252";
+                        document.getElementById(answerArray[i]).style.background = "#e25252"; 
+                    }                   
+                }
+                Answer = '';
+                currentContainer++;
+                inputIndex = 0;
+              
+            }
         }
         else if (event.target.id === 'reset-btn') {
             location.reload(true);
@@ -101,7 +139,10 @@ function buttonHandler(event) {
             }
         }
     } else if (currentContainer > MAX_CONTAINER - 1) {
-        console.log("exit");
+        var myModal = new bootstrap.Modal(document.getElementById('failModal'),{
+            keyboard: false
+          })
+        myModal.toggle()
     }
 }
  
@@ -132,5 +173,12 @@ function generateExpression() {
 }
 
 function evaluateAnswer(AnswerExp) {
-    return (AnswerExp == expression ? true : false)
+    if(eval(AnswerExp) != eval(expression)) return -1 
+    else if (AnswerExp == expression) return 1
+    else return 0
+  
+}
+
+function continueGame(){
+
 }
